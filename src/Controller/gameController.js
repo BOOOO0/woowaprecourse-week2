6ed2d8488dcb3b37ab0b1623bd0b95b3e129cf53
BOOOO0 <sunarol@naver.com>
2022-11-26 16:17:47 +0900

    refactor: apply MVC pattern to greeting

--- src/Controller/gameController.js
+++ src/Controller/gameController.js
@@ -1,3 +1,16 @@
-class gameController {}
+const BaseballGame = require("../Model/BaseballGame");
+const ExceptionHandler = require("../utils/ExceptionHandler");
+const InputView = require("../View/InputView");
+const OutputView = require("../View/OutputView");
+
+class gameController {
+  constructor() {
+    this.baseballGame = new BaseballGame();
+  }
+
+  greeting() {
+    OutputView.printGreeting();
+  }
+}
 
 module.exports = gameController;
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -1,3 +1,10 @@
-const OutputView = {};
+const Message = require("../utils/Message");
+const { Console } = require("@woowacourse/mission-utils");
+
+const OutputView = {
+  printGreeting() {
+    Console.print(Message.GREETING);
+  },
+};
 
 module.exports = OutputView;
