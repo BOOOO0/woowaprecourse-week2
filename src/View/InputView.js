e25d68277f2236e59d268cd3b84f24dfdef82d7f
BOOOO0 <sunarol@naver.com>
2022-11-26 18:10:01 +0900

    feat: create InputView and OutputView
    these Objects will get callback functions and execute them
    with input

--- src/View/InputView.js
+++ src/View/InputView.js
@@ -1,11 +1,16 @@
 const { Console } = require("@woowacourse/mission-utils");
+const Message = require("../utils/Message");
 const InputView = {
   /**
    * 사용자의 입력을 받는 메소드
    * @param {function} callback input을 사용하는 로직을 담은 콜백 함수
    */
   readUserInput(callback) {
-    Console.readLine("", callback);
+    Console.readLine(Message.ASK_GUESS_NUMBER, callback);
+  },
+
+  readReplay(callback) {
+    Console.readLine(Message.ASK_QUIT_OR_RETRY, callback);
   },
 };
 
--- src/View/OutputView.js
+++ src/View/OutputView.js
@@ -5,6 +5,22 @@ const OutputView = {
   printGreeting() {
     Console.print(Message.GREETING);
   },
+
+  printGameResult(result) {
+    Console.print(result);
+  },
+
+  printEndMessage() {
+    Console.print(Message.END_MESSAGE);
+  },
+
+  printErrorMessage(err) {
+    Console.print(err);
+  },
+
+  closeConsole() {
+    Console.close();
+  },
 };
 
 module.exports = OutputView;
