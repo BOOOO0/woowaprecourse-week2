d34641157b0fc87099b8d0e1210e4b0630f1d29f
BOOOO0 <sunarol@naver.com>
2022-11-26 17:00:16 +0900

    feat: Message's methods returns result message

--- src/utils/Message.js
+++ src/utils/Message.js
@@ -1,5 +1,18 @@
 const Message = {
   GREETING: "숫자 야구 게임을 시작합니다.",
+  ASK_GUESS_NUMBER: "숫자를 입력해주세요 : ",
+
+  returnResultBall: (ball) => {
+    return `${ball}볼\n`;
+  },
+
+  returnResultStrike: (strike) => {
+    return `${strike}스트라이크\n`;
+  },
+
+  returnResultBallAndStrike: (ball, strike) => {
+    return `${ball}볼 ${strike}스트라이크\n`;
+  },
 };
 
 module.exports = Message;
