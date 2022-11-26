0380ae698f943d54c4c775b94593163ca8ac2321
BOOOO0 <sunarol@naver.com>
2022-11-26 18:11:04 +0900

    refactor: edit throwing messages

--- src/utils/ExceptionHandler.js
+++ src/utils/ExceptionHandler.js
@@ -5,24 +5,26 @@ const ExceptionHandler = {
     const checkSet = new Set(answer.split(""));
 
     if ([...checkSet].length < USER_INPUT_LENGTH) {
-      throw new Error("[ERROR] 중복이 없는 숫자를 입력해주세요.");
+      throw "[ERROR] 중복이 없는 숫자를 입력해주세요.\n";
     }
   },
 
   checkUserInputLength(answer) {
     if (answer.length !== USER_INPUT_LENGTH) {
-      throw new Error("[ERROR] 세자리 숫자를 입력해주세요.");
+      throw "[ERROR] 세자리 숫자를 입력해주세요.\n";
     }
   },
 
   checkValidInput(answer) {
-    if (answer != REPLAY && answer != CLOSE) throw new Error("[ERROR] 유효한 숫자를 입력해주세요");
+    if (answer != REPLAY && answer != CLOSE) {
+      throw "[ERROR] 유효한 숫자를 입력해주세요\n";
+    }
   },
 
   checkInvalidDigit(answer) {
     answer.split("").forEach((digit) => {
       if (digit == INVALID) {
-        throw new Error("[ERROR] 1부터 9까지의 숫자를 입력해주세요");
+        throw "[ERROR] 1부터 9까지의 숫자를 입력해주세요\n";
       }
     });
   },
