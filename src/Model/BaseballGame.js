e5e3443b8fc9819447375cc108218fb964f8accf
BOOOO0 <sunarol@naver.com>
2022-11-26 18:14:28 +0900

    refactor: class BaseballGame only do its own job
    it just calculate ball and strike
    other jobs will be taken by GameController

--- src/Model/BaseballGame.js
+++ src/Model/BaseballGame.js
@@ -1,103 +1,49 @@
+const Constant = require("../utils/Constant");
+const { returnResultBall } = require("../utils/Message");
+
 class BaseballGame {
   #randomNumberArray;
+  #ballAndStrike;
   constructor(randomNumberArray) {
     this.#randomNumberArray = randomNumberArray;
+    this.#ballAndStrike = new Map();
   }
 
-  firstPlay() {
-    this.greeting();
-    this.computer.makeRandomNumberArray();
-    this.inputUserAnswer();
+  gamePlay(input) {
+    this.checkBall(input, this.#randomNumberArray);
+    this.checkStrike(input, this.#randomNumberArray);
   }
 
-  replay() {
-    this.computer.makeRandomNumberArray();
-    this.inputUserAnswer();
-  }
-
-  greeting() {
-    MissionUtils.Console.print("숫자 야구 게임을 시작합니다.");
-  }
-
-  inputUserAnswer() {
-    MissionUtils.Console.readLine("숫자를 입력해주세요 : ", (answer) => {
-      this.user.saveUserInput(answer);
-
-      this.exceptionHandler.checkUserInputLength(answer);
-      this.exceptionHandler.checkUserInputRedundancy(answer);
-      this.exceptionHandler.checkInvalidDigit(answer);
-
-      this.checkBall(this.computer.computerRandomNumberArray, this.user.userAnswer);
-      this.checkStrike(this.computer.computerRandomNumberArray, this.user.userAnswer);
-      this.printBallStrike();
-    });
-  }
-
-  checkBall(computerRandomNumberArray, userAnswer) {
+  checkBall(input, randomNumberArray) {
     let ball = 0;
 
-    userAnswer.split("").forEach((digit, index) => {
-      if (
-        computerRandomNumberArray.includes(parseInt(digit)) &&
-        computerRandomNumberArray.indexOf(parseInt(digit)) !== index
-      )
-        ball++;
+    input.split("").forEach((digit, index) => {
+      if (randomNumberArray.includes(parseInt(digit)) && randomNumberArray.indexOf(parseInt(digit)) !== index) ball++;
     });
 
-    return ball;
+    this.#ballAndStrike.set(Constant.BALL, ball);
   }
 
-  checkStrike(computerRandomNumberArray, userAnswer) {
+  checkStrike(input, randomNumberArray) {
     let strike = 0;
 
-    userAnswer.split("").forEach((digit, index) => {
-      if (
-        computerRandomNumberArray.includes(parseInt(digit)) &&
-        computerRandomNumberArray.indexOf(parseInt(digit)) === index
-      )
-        strike++;
+    input.split("").forEach((digit, index) => {
+      if (randomNumberArray.includes(parseInt(digit)) && randomNumberArray.indexOf(parseInt(digit)) === index) strike++;
     });
 
-    return strike;
+    this.#ballAndStrike.set(Constant.STRIKE, strike);
   }
 
-  gameContinueorEnd(strike) {
-    if (strike === STRIKE_OUT) {
-      this.initializeBallandStrike();
-
-      MissionUtils.Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-      this.askReplayorClose();
-    } else {
-      this.initializeBallandStrike();
-
-      this.inputUserAnswer();
-    }
+  returnBall() {
+    return this.#ballAndStrike.get(Constant.BALL);
   }
 
-  printBallStrike() {
-    if (this.ball === INVALID && this.strike !== INVALID) {
-      MissionUtils.Console.print(`${this.strike}스트라이크`);
-    } else if (this.strike === INVALID && this.ball !== INVALID) {
-      MissionUtils.Console.print(`${this.ball}볼`);
-    } else if (this.strike !== INVALID && this.ball !== INVALID) {
-      MissionUtils.Console.print(`${this.ball}볼 ${this.strike}스트라이크`);
-    } else {
-      MissionUtils.Console.print("낫싱");
-    }
-
-    this.gameContinueorEnd(this.strike);
+  returnStrike() {
+    return this.#ballAndStrike.get(Constant.STRIKE);
   }
 
-  askReplayorClose() {
-    MissionUtils.Console.readLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", (answer) => {
-      this.exceptionHandler.checkValidInput(answer);
-
-      if (answer == CLOSE) {
-        MissionUtils.Console.close();
-      } else if (answer == REPLAY) {
-        this.replay();
-      } else throw new Error();
-    });
+  returnBallAndStrike() {
+    return [this.#ballAndStrike.get(Constant.BALL), this.#ballAndStrike.get(Constant.STRIKE)];
   }
 }
 
